58aa9ec0478670d6595d1092e5c45608a211d446
Octávio wink <owinkk4@gmail.com>
2021-10-26 21:21:28 -0300

    palheta de cor

---
+++ main.js
@@ -0,0 +1,23 @@
+function generateColors() {
+
+    const chars = '0123456789ABCDEF'
+    let color = '#'
+
+    for (let i = 0; i < 6; i++) {
+        color += chars.charAt(Math.floor(Math.random() * chars.length))
+    }
+
+
+    return color;
+}
+
+document.getElementById('btn-action').addEventListener('click', () => {
+
+    for (let i = 0; i < 5; i++) {
+        let color = generateColors()
+        let div_color = document.getElementById(`color-${i + 1}`)
+        div_color.style.backgroundColor = color
+        let txt_color = document.getElementById(`txt-color-${i + 1}`)
+        txt_color.textContent = `${color}`
+    }
+})
